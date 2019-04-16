a8fb8db5a46abbdf9c6c3b7c5582d2f602a7c791
github username <github email address>
2019-04-16 17:13:36 -0400

    done

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,52 @@
+# pigeon_data = {
+#   :color => {
+#     :purple => ["Theo", "Peter Jr.", "Lucky"],
+#     :grey => ["Theo", "Peter Jr.", "Ms. K"],
+#     :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+#     :brown => ["Queenie", "Alex"]
+#   },
+#   :gender => {
+#     :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+#     :female => ["Queenie", "Ms. K"]
+#   },
+#   :lives => {
+#     "Subway" => ["Theo", "Queenie"],
+#     "Central Park" => ["Alex", "Ms. K", "Lucky"],
+#     "Library" => ["Peter Jr."],
+#     "City Hall" => ["Andrew"]
+#   }
+# }
+
+# pigeon_list = {
+#   "Theo" => {
+#     :color => ["purple", "grey"],
+#     :gender => ["male"],
+#     :lives => ["Subway"]
+#   },
+#   "Peter Jr." => {
+#     :color => ["purple", "grey"],
+#     :gender => ["male"],
+#     :lives => ["Library"]
+#   },
+
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash = {}
+  data.each do |color, excolor|
+    excolor.each do |purple, expurpl|
+      expurpl.each do |name|
+        if !hash.include?(name)
+          hash[name] = {}
+        end
+        if !hash[name].include?(color)
+          hash[name][color] = []
+        end
+        if !hash[name][color].include?(purple)
+          hash[name][color] << purple.to_s
+        end
+      end
+    end
+  end
+  hash
 end
